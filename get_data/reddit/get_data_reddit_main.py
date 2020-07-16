4aa64bf3e38d466e6970bbaf4c70d4ac1078108a
Josh Gillott <j.gillott@hotmail.co.uk>
2020-07-16 22:27:43 +0100

    Able to retrieve all comments on a given submission.

--- get_data/reddit/get_data_reddit_main.py
+++ get_data/reddit/get_data_reddit_main.py
@@ -1,8 +1,23 @@
 import praw
-import pprint
+from datetime import datetime
 
-user_agent="testscript by /u/SwagSorcerer420_API"
+def print_limits(reddit_instance):
+	limit_info = reddit_instance.auth.limits
+	limit_info['reset_timestamp'] = '{}{}{}'.format(str(limit_info['reset_timestamp']), ' | ', str(datetime.utcfromtimestamp(limit_info['reset_timestamp']).strftime('%Y-%m-%d %H:%M:%S')))
+	print(limit_info)
+
+
+user_agent= "testscript by /u/SwagSorcerer420_API"
 reddit = praw.Reddit(user_agent=user_agent)
 
-for submission in reddit.subreddit("all").hot(limit=25):
-	print(submission.title)
+submission_id = "hsctzt"
+submission = reddit.submission(id=submission_id)
+
+submission.comments.replace_more(limit=None)
+comment_count = 0
+for comment in submission.comments.list():
+	print(comment.body)
+	print(comment_count)
+	comment_count += 1
+
+print_limits(reddit)
