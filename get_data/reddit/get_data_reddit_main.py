71b61cb30c9aaf8524b4253798c697ee83523547
Josh Gillott <j.gillott@hotmail.co.uk>
2020-07-16 21:18:03 +0100

    Updated to modern praw implementation.

--- get_data/reddit/get_data_reddit_main.py
+++ get_data/reddit/get_data_reddit_main.py
@@ -1,5 +1,8 @@
 import praw
+import pprint
 
-reddit = praw.Reddit(user_agent="testscript by /u/SwagSorcerer420_API")
+user_agent="testscript by /u/SwagSorcerer420_API"
+reddit = praw.Reddit(user_agent=user_agent)
 
-
+for submission in reddit.subreddit("all").hot(limit=25):
+	print(submission.title)
