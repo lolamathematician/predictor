fccd56b7ab5ccea07698af45fd16399d91e6e940
Josh Gillott <j.gillott@hotmail.co.uk>
2020-07-26 12:04:53 +0100

    Changed order of retrieval and unpacking comments and submissions to unpack comments durint API reset to retrieve submissions.

--- get_data/reddit/get_data_reddit_main.py
+++ get_data/reddit/get_data_reddit_main.py
@@ -3,8 +3,8 @@ from datetime import datetime, timedelta
 import time
 
 # NUMBER_OF_COMMENTS_TO_RETRIEVE = 10 # DEPRECATED
-START_DATE = datetime(year=2020, month=3, day=20)
-END_DATE = datetime(year=2020, month=5, day=31) # Inclusive
+START_DATE = datetime(year=2020, month=7, day=11)
+END_DATE = datetime(year=2020, month=7, day=25) # Inclusive
 ONE_DAY = timedelta(days=1)
 
 def convert_utc_to_readable_time(utc_time):
@@ -27,8 +27,8 @@ def retrieve_results(api, batch_date):
 	batch_start_epoch = int(batch_date.timestamp())
 	batch_end_epoch = int(batch_end_date.timestamp())
 	comment_results = api.search_comments(subreddit='wallstreetbets', after=batch_start_epoch, before=batch_end_epoch)
-	submission_results = api.search_submissions(subreddit='wallstreetbets', after=batch_start_epoch, before=batch_end_epoch)
 	comment_results = [comment_result.d_ for comment_result in comment_results]
+	submission_results = api.search_submissions(subreddit='wallstreetbets', after=batch_start_epoch, before=batch_end_epoch)
 	submission_results = [submission_result.d_ for submission_result in submission_results]
 	print('{current_time} {results_date} Results retrieved.'.format(current_time=current_time, results_date=date_string))
 	return comment_results, submission_results
