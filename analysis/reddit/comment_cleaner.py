d19d35a70ab4af75dacdeece020c034982a36baf
Joe <psxjr4@nottingham.ac.uk>
2020-07-26 17:20:47 +0100

    Added constructors for text processing. incorporated config file for easy generation and customisation of pipeline

--- analysis/reddit/comment_cleaner.py
+++ analysis/reddit/comment_cleaner.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import ahocorasick
 from math import nan
+import configparser
 
 tickers = {0: "TSLA",
            1: "MSFT"}
@@ -16,41 +17,25 @@ NO_TICKER_ID = "NO_TICKER"
 
 
 class TickerSearch:
-    def __init__(self):
-        self.automaton = None
-
-    def configure(self, ticker_dict):
+    def __init__(self, ticker_list):
+        self.ticker_list = ticker_list
         self.automaton = ahocorasick.Automaton()
-        for ticker_index in tickers.keys():
-            self.automaton.add_word(ticker_dict[ticker_index], ticker_index)
+        for ticker_index, ticker in enumerate(ticker_list):
+            self.automaton.add_word(ticker, ticker_index)
         self.automaton.make_automaton()
 
     def search(self, messy_text_body):
-        tickers_in_text = [tickers[ticker_index] for (_, ticker_index) in self.automaton.iter(messy_text_body)]
+        tickers_in_text = [self.ticker_list[ticker_index] for (_, ticker_index) in self.automaton.iter(messy_text_body)]
         if not tickers_in_text:
             return NO_TICKER_ID
         return tickers_in_text
 
 
 class TextCleaner:
-    def __init__(self):
-        self.stopwords = None
-        self.tokeniser = None
-        self.lemmatiser = None
-
-    def configure(self, tokeniser=None, lemmatiser=None):
+    def __init__(self, tokeniser, lemmatiser):
         self.stopwords = stopwords.words('english')
-        if tokeniser is None:
-            # Removes punctuation as well as tokenising
-            self.tokeniser = RegexpTokenizer(r'\w+')
-        else:
-            self.tokeniser = tokeniser
-
-        if lemmatiser is None:
-            self.lemmatiser = WordNetLemmatizer()
-            # converts all words to one form of similar meaning i.e. rocks -> rock, better -> good
-        else:
-            self.lemmatiser = lemmatiser
+        self.tokeniser = tokeniser
+        self.lemmatiser = lemmatiser
 
     def clean(self, text):
         return " ".join([self.lemmatiser.lemmatize(token.lower()) for token in self._tokenise(text) if token.lower()
@@ -63,9 +48,6 @@ class TextCleaner:
 class ScoreExtractor(ABC):
     @abstractmethod
     def __call__(self, score_dict):
-        pass
-
-    def configure(self, **kwargs):
         pass
 
 
@@ -120,7 +102,8 @@ class DefaultVaderAnalyser(SentimentAnalyser):
 
 
 class TextProcessor:
-    def configure(self, ticker_searcher: TickerSearch, cleaner: TextCleaner, analyser: SentimentAnalyser, score_extractor: ScoreExtractor):
+    def __init__(self, ticker_searcher: TickerSearch, cleaner: TextCleaner,
+                 analyser: SentimentAnalyser, score_extractor: ScoreExtractor):
         self.ticker_searcher = ticker_searcher
         self.cleaner = cleaner
         self.analyser = analyser
@@ -134,7 +117,68 @@ class TextProcessor:
         return {"id": id, "sentiment_score": sentiment_score, "tickers": "_".join(tickers_in_text)}
 
 
-if __name__ == "__main__":
+def ticker_search_constructor(key):
+    available_tickers = ["TSLA", "MSFT", "APPL"]  # ETC
+    if key == "ALL":
+        return TickerSearch(available_tickers)
+    tickers_from_key = key.split(",")
+    for ticker in tickers_from_key:
+        if ticker not in available_tickers:
+            raise KeyError("{} is not a valid ticker!".format(ticker))
+    return TickerSearch(tickers_from_key)
+
+
+def tokeniser_constructor(key):
+    tokeniser_dict = {"REGEXP_WORDS": RegexpTokenizer(r'\w+')}
+    try:
+        return tokeniser_dict[key]
+    except KeyError as error:
+        raise
+
+
+def lemmatiser_constructor(key):
+    lemmatiser_dict = {"WORDNET": WordNetLemmatizer()}
+    try:
+        return lemmatiser_dict[key]
+    except KeyError as error:
+        raise
+
+
+def scorer_constructor(key):
+    # TODO INCORPORATE WEIGHTS
+    scorer_dict = {"DIRAC": DiracScore(),
+                   "COMPOUND": CompoundScore(),
+                   "POSITIVE": PositiveScore(),
+                   "NEGATIVE": NegativeScore(),
+                   "NEUTRAL": NeutralScore()}
+    try:
+        return scorer_dict[key]
+    except KeyError as error:
+        raise
+
+
+def analyser_constructor(key):
+    anaylser_dict = {"DEFAULT_VADER": DefaultVaderAnalyser()}
+    try:
+        return anaylser_dict[key]
+    except KeyError as error:
+        raise
+
+
+def construct_text_processing_pipeline(mode):
+    config = configparser.ConfigParser()
+    config.read('./resources/text_processing.ini')
+    process_cfg = config[mode]
+    ticker_searcher = ticker_search_constructor(process_cfg["TICKERS"])
+    tokeniser = tokeniser_constructor(process_cfg["TOKENISER"])
+    lemmatiser = lemmatiser_constructor(process_cfg["LEMMATISER"])
+    scorer = scorer_constructor(process_cfg["SCORER"])
+    analyser = analyser_constructor(process_cfg["ANALYSER"])
+    cleaner = TextCleaner(tokeniser=tokeniser, lemmatiser = lemmatiser)
+    return TextProcessor(ticker_searcher=ticker_searcher, cleaner=cleaner, analyser=analyser, score_extractor=scorer)
+
+
+def test(mode):
     no_ticker_text = "Well I actually bought 100 shares on Friday. Sold a covered call 0dte for $5 and also sold 2 cash covered  0dte puts for about $45. \n\nThe puts expired about .15 away from the money and werenโ€t exercised so made more money just selling puts and will continue doing that."
     no_ticker_id = "no_ticker_id"
 
@@ -143,14 +187,11 @@ if __name__ == "__main__":
 
     text_and_ids = [(no_ticker_text, no_ticker_id), (ticker_text, ticker_id)]
 
-    cleaner = TextCleaner()
-    cleaner.configure()
-    score_extractor = CompoundScore()
-    sentiment_analyser = DefaultVaderAnalyser()
-    ticker_search = TickerSearch()
-    ticker_search.configure(tickers)
-
-    processor = TextProcessor()
-    processor.configure(ticker_search, cleaner, sentiment_analyser, score_extractor)
+    test_processor = construct_text_processing_pipeline(mode)
     for text, id in text_and_ids:
-        print(processor.process(text, id))
+        print(test_processor.process(text, id))
+
+
+if __name__ == "__main__":
+    test("STANDARD")
+
